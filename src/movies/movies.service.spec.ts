a63fbdd9a4356c15ca9083263db16bdf77861aa8
baekjung <zvgandam@gmail.com>
2021-05-21 16:17:27 +0900

    jest test

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -14,5 +15,100 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getMovies', () => {
+    it('should return an array', () => {
+      const movies = service.getMovies();
+      expect(movies).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getMovie', () => {
+    it('should return a movie', () => {
+      service.createMovie({
+        title: 'Test Movie',
+        director: 'tester',
+        year: 2021,
+        genres: ['test'],
+      });
+      const movie = service.getMovie(1);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(1);
+    });
+
+    it('should throw a NotFoundException', () => {
+      try {
+        service.getMovie(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Not Found Movie ID 999');
+      }
+    });
+  });
+
+  describe('deleteMovie', () => {
+    it('deletes a movie', () => {
+      service.createMovie({
+        title: 'Test Movie',
+        director: 'tester',
+        year: 2021,
+        genres: ['test'],
+      });
+      const beforeDeleteLength = service.getMovies().length;
+      service.deleteMovie(1);
+      const afterDeleteLength = service.getMovies().length;
+
+      expect(afterDeleteLength).toBeLessThan(beforeDeleteLength);
+    });
+
+    it('should throw a NotFoundException', () => {
+      try {
+        service.deleteMovie(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Not Found Movie ID 999');
+      }
+    });
+  });
+
+  describe('createMovie', () => {
+    it('should carete a movie', () => {
+      const beforeCreateLength = service.getMovies().length;
+      service.createMovie({
+        title: 'Test Movie',
+        director: 'tester',
+        year: 2021,
+        genres: ['test'],
+      });
+      const afterCreateLength = service.getMovies().length;
+
+      expect(afterCreateLength).toBeGreaterThan(beforeCreateLength);
+    });
+  });
+
+  describe('updateMovie', () => {
+    it('should update a movie', () => {
+      service.createMovie({
+        title: 'Test Movie',
+        director: 'tester',
+        year: 2021,
+        genres: ['test'],
+      });
+
+      service.updateMovie(1, { year: 2022 });
+      const movie = service.getMovie(1);
+
+      expect(movie.year).toEqual(2022);
+    });
+
+    it('should throw a NotFoundException', () => {
+      try {
+        service.updateMovie(999, { year: 2022 });
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Not Found Movie ID 999');
+      }
+    });
   });
 });
